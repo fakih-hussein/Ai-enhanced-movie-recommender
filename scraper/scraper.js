471d773696c29b1f8393e678e34c08d4bf8023b6
amjadsh92 <amjadsharafeddien@gmail.com>
2024-11-10 16:33:44 +0200

    scrape duration and genre of each movie

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -2,22 +2,35 @@ import puppeteer from "puppeteer";
 
 const browser = await puppeteer.launch({
   headless: true,
+  browser:'firefox'
+  
 });
 const page = await browser.newPage();
 
+const firefox = {
+     "Sec-Fetch-Dest" : "document",
+     "Sec-Fetch-Mode": "navigate",
+     "Sec-Fetch-Site": "none", 
+     "Sec-Fetch-User": "?1",
+     "Upgrade-Insecure-Requests":"1",
+     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0"
 
-await page.setExtraHTTPHeaders({
+}
+
+const chrome = {
     "sec-ch-ua": `"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"`,
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "Linux",
     "User-Agent":
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
-  });
+  }
+await page.setExtraHTTPHeaders(firefox);
   
   await page.goto("https://www.pathe.fr/cinemas/cinema-pathe-wilson");
   
-  await page.waitForSelector(".row .card-screening .h3 a", { timeout: 1000000 });
+  await page.waitForSelector(".row .card-screening .h3 a");
   const nameNodes = await page.$$(".row .card-screening .h3 a");
+  
   const linkTitles = []
   for (let nameNode of nameNodes) {
     
@@ -25,6 +38,7 @@ await page.setExtraHTTPHeaders({
       return {
         link: el.getAttribute("href"),
         name: el.innerHTML,
+        
       };
     }, nameNode);
 
@@ -41,6 +55,22 @@ await page.setExtraHTTPHeaders({
     //console.log(domain+linkTitle.link);
 
     await page.goto(domain+linkTitle.link);
+
+    await page.waitForSelector(".container .hero-film__content .ft-default")  
+
+    const durationNode  = await page.$$(".container .hero-film__content .ft-default")
+    
+    
+    
+    const duration = await page.evaluate((el) => el.innerHTML, durationNode[0])
+
+    const genre = await page.evaluate((el) => el.innerHTML, durationNode[1])
+     
+
+    //console.log(duration)
+
+
+    
 
     await page.waitForSelector(".container .hero-film__content .hero-film__body a[role='button']");
 
