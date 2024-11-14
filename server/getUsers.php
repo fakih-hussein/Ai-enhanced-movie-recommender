c0ea26ebb86d887a8994740afd10a3b803a0efcc
fh95-tech <fakih.hussein995@gmail.com>
2024-11-14 06:15:21 +0200

    added get users api

---
+++ server/getUsers.php
@@ -0,0 +1,20 @@
+<?php
+header("Content-Type: application/json");
+
+$host = "localhost";
+$dbname = "movie_recommender_db";
+$dbuser = "root";
+$dbpass = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $pdo->prepare("SELECT * FROM users");
+    $stmt->execute();
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($users);
+} catch (PDOException $e) {
+    echo json_encode(["error" => "Database error: " . $e->getMessage()]);
+}
